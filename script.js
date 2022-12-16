6f5574f12ba79211a8cfe849d9528a97b7c532fb
Mathboss42 <115639944+Mathboss42@users.noreply.github.com>
2022-12-16 20:40:18 +0100

    Set up functions to add markers on board

--- script.js
+++ script.js
@@ -16,18 +16,19 @@ const menu = (() => {
 
 const game = (() => {
     let playerList = [];
-    let currentTurn = '';
+    let currentPlayer;
     const markers = ['X', 'O']
 
     const start = () => {
         console.log('start');
         createPlayers();
-        gameBoard.initGrid();
-        console.log(gameBoard.domGrid);
+        setCurrentPlayer();
+        gameBoard.initGameBoard();
     }
     
     const restart = () => {
         console.log('restart');
+        setCurrentPlayer();
     }
     
     const quit = () => {
@@ -38,18 +39,51 @@ const game = (() => {
     const createPlayers = () => {
         if (playerList.length === 0) {
             for (let i = 0; i < 2; i++) {    
-                const newPlayer = Player(
-                    `Player ${i+1}`,
-                    markers[i],
-                    gameBoard.domGrid
-                    );
+                const newPlayer = Player(`Player ${i+1}`, markers[i]);
                 playerList.push(newPlayer);
             }
         }
-        console.log(playerList);
+        // console.log(playerList);
+    }
+
+    const setCurrentPlayer = () => {
+        if (currentPlayer === undefined) {
+            currentPlayer = playerList[0];
+            currentPlayer.isMyTurn = !(currentPlayer.isMyTurn)
+        } else {
+            if (currentPlayer === playerList[0]) {
+                currentPlayer = playerList[1];
+                playerList.forEach(el => el.isMyTurn = !(el.isMyTurn));
+            } else {
+                currentPlayer = playerList[0];
+                playerList.forEach(el => el.isMyTurn = !(el.isMyTurn));
+            }
+        }
+    }
+
+    const placeMarker = (e) => {
+        const x = e.target.dataset.x;
+        const y = e.target.dataset.y;
+
+        // console.log(`clicked tile x = ${x}, y = ${y}`);
+        if (!(isMoveLegal(x, y))) {
+            return;
+        } else {
+            gameBoard.update(x, y, currentPlayer.marker);
+            setCurrentPlayer();
+            // console.log('place marker');
+        }
+    }
+
+    const isMoveLegal = (x, y) => {
+        if (gameBoard.getCurrentCell(x, y).innerHTML === '') {
+            return true;
+        } else {
+            return false;
+        }
     }
     
-    return {start, restart, quit};
+    return {start, restart, quit, placeMarker};
 })();
 
 
@@ -60,32 +94,55 @@ const gameBoard = (() => {
         ['', '', ''],
         ['', '', '']
     ];
-
-    //cache dom
-    const initGrid = () => {    
-        const domGrid = document.querySelectorAll('.grid-cell');
-        console.log('domgrid = ' + domGrid);
-        // domGrid.forEach(el => addEventListener('click', () => {}))
-        return domGrid;
+    
+    const domGrid = document.querySelectorAll('.grid-cell');
+    
+    const initGameBoard = () => {
+        for (let i = 0; i < domGrid.length; i++) {
+            domGrid[i].addEventListener('click', game.placeMarker);
+        }
     }
 
-    const updateDisplay = () => {
-        return;
+    const getCurrentCell = (x, y) => {
+        let currentCell;
+        for (let i = 0; i < domGrid.length; i++) {
+            if ((domGrid[i].dataset.x == x) && (domGrid[i].dataset.y == y)) {
+                currentCell = domGrid[i];
+            }
+        }
+        return currentCell;
     }
 
-    return {updateDisplay, initGrid}
+    const update = (x, y, marker) => {
+        updateGameBoard(x, y, marker);
+        updateDisplay(x, y, marker);
+        checkForWin();
+    }
+
+    const updateGameBoard = (x, y, marker) => {
+        gameBoard[x][y] = marker;
+        console.log(gameBoard);
+    }
+
+    const updateDisplay = (x, y, marker) => {
+        console.log('updateDisplay');
+        getCurrentCell(x, y).innerHTML = marker;
+    }
+
+    const checkForWin = () => {
+        console.log('check for win');
+        gameBoard.forEach(el => console.log(el.includes('')));
+    }
+
+    return {initGameBoard, getCurrentCell, update};
 })();
 
 
 
-const Player = (name, marker, grid) => {
+const Player = (name, marker) => {
     let isMyTurn = false;
 
-    const placeMarker = () => {
-        
-    }
-
-    return {name, marker, grid};
+    return {name, marker, isMyTurn};
 }
 
-console.log(gameBoard.initGrid());
+// gameBoard.initGameBoard();
